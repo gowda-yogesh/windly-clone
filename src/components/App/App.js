9d0b0ca2660a0a336ac1080cf1b5c48f5e0d5e12
Yogesh G <yogesh.g@nabler.com>
2021-05-25 00:24:37 +0530

    WMS maps working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,36 @@
 import React, { Component } from "react";
-import "./App.css"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  WMSTileLayer,
+  
+} from "react-leaflet";
 
+import MapView from "../MapView/MapView.js";
 
 class App extends Component {
   render() {
+    const wmsURL = "http://ows.mundialis.de/services/service?";
+    const wmsURL2 = "https://thredds.socib.es/thredds/wms/observational/hf_radar/hf_radar_ibiza-scb_codarssproc001_L1_agg/hf_radar_ibiza-scb_codarssproc001_L1_agg_best.ncd";
+    const layerOption = "TOPO-OSM-WMS";
+    const layerOption2 ="sea_water_velocity" ;
+
+
+    
     return (
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <MapContainer
+        center={[12.9716, 77.5946]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <WMSTileLayer url={wmsURL} layers={layerOption} ></WMSTileLayer>
+      </MapContainer>
     );
   }
 }
-
-
 
 export default App;
---
+++ src/components/MapView/MapView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MapView extends Component {
+    render() {
+        return (
+
+            <div id="map" class="map"></div>
+
+        );
+    }
+}
+
+export default MapView;
